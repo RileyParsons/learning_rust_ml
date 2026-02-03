baa0042c9b64de87f3a36bab6926374e71ee6d14
Elysian <100543055+RileyParsons@users.noreply.github.com>
2026-02-03 17:46:17 +1100

    Init

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use rand::prelude::*;
+
+// links and references
+// https://docs.rs/linfa/latest/linfa/
+// https://docs.rs/rand/latest/rand/
+
+// cargo build once dependencies have been added to cargo.toml file
+// cargo run to build and compile
+// cargo check to compile code without creating exe. good for error checking
+
+// iris
+// samples: 150
+// features: 4
+// target: 1
+
+fn main() {
+    println!("Playing with Rust ML with iris dataset");
+
+    let mut rng = rand::thread_rng();
+    // need to create my own random number generator for rust to use and have it passed via mutale reference
+    let original_data = linfa_datasets::iris().shuffle(&mut rng);
+    let (train, test) = original_data.split_with_ratio(0.8);
+
+    // print via formated string
+    // {:?} and {:#?} are debug print
+    println!("Train: {:#?}",train);
+    println!("test: {:#?}",test);
+
+}
